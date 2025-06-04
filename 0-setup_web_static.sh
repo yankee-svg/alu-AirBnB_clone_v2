70ce9788310811c796b0289ece38f7cb1c5827b7
yankee-svg <e.barame@alustudent.com>
2025-06-04 04:25:33 +0000

    executable

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,36 +1,20 @@
+
 #!/usr/bin/env bash
-# script that sets up web servers for the deployment of web_static
-sudo apt-get update
+# Script that sets up your web servers for the deployment of web_static
+
+
+sudo apt-get -y update
 sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+sudo service nginx start
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-# Add test HTML file to the test folder
-sudo tee /data/web_static/releases/test/index.html > /dev/null <<EOF
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF
-
-# Create a symbolic link to /data/web_static/current
-sudo ln -sfn /data/web_static/releases/test/ /data/web_static/current
-
-# Give ownership of the /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-# Update the Nginx configuration to serve the content
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/; }' /etc/nginx/sites-enabled/default
+sudo sed -i '44i \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}' /etc/nginx/sites-available/default
 
-# Test the Nginx configuration to ensure there are no syntax errors
-sudo nginx -t
-
-# Restart Nginx to apply the changes
 sudo service nginx restart
 
